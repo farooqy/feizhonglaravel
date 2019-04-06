50fcc84498ec1566a9b527186f689570d687f47e
Noor <noorfarooqy@gmail.com>
2019-04-06 12:22:16 -0700

    Create tokenStatusGeneratorModel.php

---
+++ app/models/tokenStatusGeneratorModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tokenStatusGeneratorModel extends Model
+{
+    //
+    protected $table = 'status_token_generator';
+    protected $fillable = ["generated_token", "generated_for_id", 
+    "generated_for_type", "generated_for_token", "generated_completed"];
+
+    public function statuses()
+    {
+    	return $this->hasOne('App\models\compStatusModel', 'status_generated_token');
+    }
+    public function files()
+    {
+    	return $this->hasMany('App\models\uploadFilesModel', 'file_generated_token');
+    }
+}
