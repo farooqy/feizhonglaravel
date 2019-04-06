a11eadb895369edc9dc84f6e2b864a50173f8d3e
Noor <noorfarooqy@gmail.com>
2019-04-06 12:22:19 -0700

    Update uploadedFilesModel.php

--- app/models/uploadedFilesModel.php
+++ app/models/uploadedFilesModel.php
@@ -8,5 +8,11 @@ class uploadedFilesModel extends Model
 {
     //
 
-    protected $table = "table_uploaded_files";
+    protected $table = "uploaded_files";
+
+    public function Statuses()
+    {
+    	return $this->belongsTo('App\models\compStatusModel', 'file_generated_token','status_generated_token');
+    }
+   
 }
