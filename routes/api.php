122697221d21b504af49153a39997ec09f1a050e
Noor <noorfarooqy@gmail.com>
2019-04-07 10:15:35 -0700

    Route update for products

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,10 @@ Route::post('/comp/status/setStatus', 'statusController@setStatus');
 Route::post('/comp/status/addFile', 'statusController@uploadFile');
 Route::post('/comp/status/getToken', 'Status_TokenGeneratorModelController@generate_Token');
 
+Route::post('/comp/products', 'products\productController@getProducts');
+Route::post('/comp/product/addProduct', 'products\productController@newProduct');
+Route::post('/comp/product/addImage', 'products\productController@addProductImage');
+Route::post('/comp/product/genToken', 'Status_TokenGeneratorModelController@generate_Token');
 
 Route::post('message/history', 'chatController@getHistory');
 Route::get('message/history', 'chatController@getHistory');
