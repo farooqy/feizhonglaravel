41157665bda5741109750f6171b8d3cd75d49ff1
farooqy_ubuntu <noorfarooqy@gmail.com>
2019-08-05 16:27:43 +0800

    change the version to seven

--- app/Http/Controllers/generalController.php
+++ app/Http/Controllers/generalController.php
@@ -109,7 +109,7 @@ class generalController extends Controller
 
     public function getVersion()
     {
-        $this->Error->setSuccess(["version_number" => 0]);
+        $this->Error->setSuccess(["version_number" => 7]);
         return $this->Error->getSuccess();
     }
 }
