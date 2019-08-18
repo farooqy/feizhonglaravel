037d197c793e3ef42e1feb18fb761b49a5858f54
farooqy <noorfarooqy@gmail.com>
2019-08-18 16:17:06 -0400

    push atoc site files

--- routes/web.php
+++ routes/web.php
@@ -11,59 +11,7 @@
 |
 */
 
-Route::put('*', 'generalController@notAllowedMethod');
-Route::post('/user/login', 'user\accountController@userLogin');
-Route::post('/user/register', 'user\accountController@register');
 
-Route::post('/comp/login', 'companies\accountController@companyLogin');
-Route::post('/comp/register/basicInfo', 'companies\accountController@basicInfoRegister');
-Route::post('/comp/register/address', 'companies\accountController@addressRegistration');
-Route::post('/comp/register/type', 'companies\accountController@companyTypeRegistration');
-Route::post('/comp/verifyComp', 'companies\accountController@sendConfirmationText');
-
-
-Route::post('/comp/status', 'statusController@index');
-
-Route::post('/comp/status/getStatus', 'statusController@getStatus');
-Route::post('/comp/status/setStatus', 'statusController@setStatus');
-Route::post('/comp/status/addFile', 'statusController@uploadFile');
-Route::post('/comp/status/getToken', 'Status_TokenGeneratorModelController@generate_Token');
-Route::post('/comp/status/comment', 'statusController@writeComment');
-Route::post('/comp/status/like', 'statusController@likeStatus');
-
-Route::post('/comp/products', 'products\productController@getProducts');
-Route::post('/comp/product/addProduct', 'products\productController@newProduct');
-Route::post('/comp/product/addImage', 'products\productController@addProductImage');
-Route::post('/comp/product/genToken', 'Status_TokenGeneratorModelController@generate_Token');
-
-Route::post('/comp/featureCompany', 'companies\featuredCompaniesController@requestToBeFeatured');
-Route::post('/comp/getFeaturedImages', 'companies\featuredCompaniesController@getFeaturedCompanies');
-Route::post('/comp/allowFeatureCompany', 'companies\featuredCompaniesController@approveFeatureCompany');
-
-Route::post('/comp/quotation/generate', 'companies\quotationController@generateQuotation');
-
-Route::post('/comp/edit/name', 'companies\accountController@updateCompName');
-Route::post('/comp/edit/profile', 'companies\accountController@updateCompProfile');
-Route::post('/comp/edit/password', 'companies\accountController@updateCompPassword');
-Route::post('/comp/edit/phone', 'companies\accountController@updateCompPhone');
-Route::post('/comp/edit/addressOne', 'companies\accountController@updateCompAddressOne');
-Route::post('/comp/edit/addressTwo', 'companies\accountController@updateCompAddressTwo');
-Route::post('/comp/edit/city', 'companies\accountController@updateCompCity');
-Route::post('/comp/edit/province', 'companies\accountController@updateCompProvince');
-Route::post('/comp/edit/email', 'companies\accountController@updateCompEmail');
-Route::post('/comp/edit/description', 'companies\accountController@updateCompDescription');
-
-Route::post('/favorites/doFavorite', 'favoritesController@favoriteCompany');
-Route::post('/favorites/getFavorites', 'favoritesController@getFavorites');
-Route::post('/favorites/unFavorite', 'favoritesController@removeFavorite');
-
-Route::post('message/history', 'chatController@getHistory');
-Route::post('message/history', 'chatController@getHistory');
-Route::post('message/send', 'chatController@sendMessage');
-
-Route::post('/list/companies', 'generalController@listCompanies');
-
-
-Route::get('/quote', 'generalController@showQuote');
-
-Route::post('/images/base64', 'generalController@getBase64Image');
+Route::get('/', function () {
+    return view('index');
+});
