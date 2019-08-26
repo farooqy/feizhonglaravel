5547923927fdba67a753c032f9593a44439272fe
farooqy_ubuntu <noorfarooqy@gmail.com>
2019-08-26 17:30:29 +0800

    at cpmment  model host_token

--- database/migrations/2019_04_17_033736_create_status_comments_table.php
+++ database/migrations/2019_04_17_033736_create_status_comments_table.php
@@ -17,6 +17,7 @@ class CreateStatusCommentsTable extends Migration
             $table->increments('id');
             $table->bigInteger('status_id');
             $table->bigInteger('host_id');
+            $table->string('host_token');
             $table->string('host_type',10);
             $table->longText('comment_text',1000);
             $table->boolean('is_active')->default(true);
