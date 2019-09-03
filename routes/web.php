4ffff0a1c878275c315f5628ec7b39d2d95b70c5
farooqy <noorfarooqy@gmail.com>
2019-09-03 01:23:57 -0400

    push exhibition and whatsapp

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,7 @@
 Route::get('/', function () {
     return view('index');
 });
-<<<<<<< HEAD
 Route::get('/exhbition', 'generalController@getExhbitionPdf');
 Route::get('/whatsapp', 'generalController@whatsAppLink');
-=======
 
 Route::get('/download', 'generalController@webDownloadPage');
->>>>>>> 118a6ee891ab36af38e97be4f392fba849a85e75
