d68373de6b3bf846a711b84f107170b54c238585
noorfarooqy <noorfarooqy@gmail.com>
2020-08-06 15:50:51 +0400

    host type to integer

--- app/Http/Controllers/misc/SupplierCountryController.php
+++ app/Http/Controllers/misc/SupplierCountryController.php
@@ -48,7 +48,7 @@ class SupplierCountryController extends Controller
         $preferredLocations = SupplierCountryPrefereneModel::where([
             ["host_id", $request->host_id],
             ["host_token", $request->host_token],
-            ["host_type", $request->host_type]
+            ["host_type", $request->host_type == "normal" ? 2 : ($request->host_type == "comp" ? 1 : 3)]
         ])->get();
         if ($preferredLocations == null || $preferredLocations->count() <= 0) {
             $preferredLocations = ["prefered_supplier_location" => 0];
